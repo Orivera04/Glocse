dcf95a95e5accb6c43acb3fd10206bc5e9244104
Oscar Rivera <riveramoreira04@gmail.com>
2020-08-23 20:44:48 -0600

    Feature #1 : Deploy Inicial del Proyecto     - Se crea el repositorio.     - Se añaden todos los archivos del proyecto.

---
+++ GLOCSE/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace GLOCSE
+{
+    public class BundleConfig
+    {        
+        public static void RegisterBundles(BundleCollection bundles)
+        {            
+             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Estilos.css"));                                  
+        }
+    }
+}
---
+++ GLOCSE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GLOCSE.MvcApplication" Language="C#" %>
